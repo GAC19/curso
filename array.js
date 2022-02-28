bdcb6c87fd562d493068cfb7bf2a240dd97c8441
GAC19 <gacevedo.clavel02@gmail.com>
2022-02-28 17:26:05 -0300

    version 1.2

---
+++ array.js
@@ -0,0 +1,20 @@
+const datos=["gasfiteria","rio calle 258"];
+console.log(datos)
+
+
+const EMP=[
+    {
+        // id=0,
+        nombre="constructora",
+        direccion="maule 123",
+    },
+    {
+        // id=1,
+        nombre="panadereria",
+        direccion="arturo 123",
+    }
+
+]
+
+
+// console.log(EMP)
