2bfc28036d444e5b2da63cefb9db1902925176ec
GAC19 <gacevedo.clavel02@gmail.com>
2022-06-09 22:10:13 -0400

    evento

--- evento.js
+++ evento.js
@@ -1,11 +1,11 @@
 /*
-onblur(): se pierde el foco
+onblur(): cuando precionas fuera del explorador se hable una ventana
 onchange(): al modificar el valor del componente
 onbeforeunload(): se lanza al intenter cerrar una ventana
 onclick(): al hacer clic en un elemento
 ondblclick(): al hacer un doble click
 onerror(): si se produce un error al cargar un apaguina web
-onfocus(): cuando un componente obtiene un foco
+onfocus(): cuando selecciono o pulso donde quiero
 onkeydown(): al pulsar cualquier tecla
 onkeypress(): al pulsar una tecla corresondiente a un caracter
 onkeyup(): al levantar cualquier una tecla
@@ -25,6 +25,9 @@ const youtube=document.querySelector('#youtube')
 const cambio=document.querySelector('#cambio')
 const cambio2=document.querySelector('#cambio2')
 const cuerpo=document.querySelector('body')
+const formulario=document.querySelector('#formulario')
+const boton=document.querySelector('#pulsar')
+
     
 google.addEventListener('click',()=>{
     window.open('https://www.google.com')
@@ -36,8 +39,22 @@ youtube.addEventListener('click',()=>{
 
 cambio.addEventListener('click',()=>{
     cuerpo.style.backgroundColor='green'
+    cambio.style.backgroundColor='green'
+    cambio.style.color='yellow'
 })
 
 cambio2.addEventListener('click',()=>{
     cuerpo.style.backgroundColor='blue'
+    // alert('has cambiado el fondo de color azul')
+})
+
+window.addEventListener('blur',(e)=>{
+    cuerpo.style.backgroundColor='orange'
+    // alert('has redimensionado la ventana')
+})
+
+formulario.addEventListener('keydown',(e)=>{
+    formulario.style.backgroundColor='orangered'
+    boton.style.backgroundColor='red'
+
 })
